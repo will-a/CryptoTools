6fabfb41cbb8f349fb96e8eddc1fb7a18c10435d
wsa3263 <wsa3263@rit.edu>
2020-10-14 16:00:06 -0400

    added function to find additive and multiplicative inverses, optimized to improve runtime significantly (O(n^2) -> ~O(n))

--- inverse_tables.py
+++ inverse_tables.py
@@ -2,6 +2,49 @@ import math
 import argparse
 
 
+def extendedGcd(r0, r1):
+    """
+    Find extended GCD of r0, r1
+    returns GCD(r0, r1), s0 and t0 such that r0*s0 + r1*t0 = GCD(r0, r1)
+    """
+    s0 = 1
+    s1 = 0
+    t0 = 0
+    t1 = 1
+    while r1 != 0:
+        q = r0 // r1
+        s1, s0 = s0 - q * s1, s1
+        t1, t0 = t0 - q * t1, t1
+        r0, r1 = r1, r0 % r1
+    return r0, s0, t0
+
+
+def find_add_inv(ring_size: int) -> dict:
+    d = dict()
+    for i in range(ring_size):
+        d[i] = ring_size - i
+    return d
+
+
+def find_mul_inv(ring_size: int) -> dict:
+    d = dict()
+    for i in range(ring_size):
+        res, x, y = extendedGcd(i, ring_size)
+        if res == 1:
+            d[i] = (x % ring_size + ring_size) % ring_size
+    return d
+
+
+def print_inv_dict(d: dict) -> None:
+    ring_size = list(d.keys())[-1] + 1
+    max_num_digits = int(math.log10(ring_size - 1)) + 1
+    print('{:>{width}} | Inverse'.format("Value", width=max_num_digits))
+    for k in d.keys():
+        if d[k] != []:
+            print('{:>{width}} | {}'.format(k, d[k], width=max_num_digits + 4))
+    print()
+
+
 def add_inv_table(ring_size: int) -> None:
     max_num_digits = int(math.log10(ring_size - 1)) + 1
     col_header = ""
@@ -45,7 +88,10 @@ def main():
     if parsed_args.ringsize <= 0:
         print("Ring size must be greater than zero.")
         return
-
+    if parsed_args.a:
+        print_inv_dict(find_add_inv(parsed_args.ringsize))
+    if parsed_args.m:
+        print_inv_dict(find_mul_inv(parsed_args.ringsize))
     if parsed_args.a and parsed_args.t:
         add_inv_table(parsed_args.ringsize)
     if parsed_args.m and parsed_args.t:
